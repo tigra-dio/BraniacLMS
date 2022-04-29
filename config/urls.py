39c3689f0a928735d31a033c5f5fdda3052fcb29
tigra_dio <tigra_dio@mail.ru>
2022-04-29 18:13:15 +0300

    Система адресации

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mainapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.hello_world),
+    path('<str:word>/', views.check_kwargs),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    return HttpResponse("Hello world!")
+
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse(f"kwargs:<br>{kwargs}")
