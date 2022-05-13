e1f74cc8909917aa7be990e44f89a1ff69d1556d
tigra_dio <tigra_dio@mail.ru>
2022-05-13 14:55:07 +0300

    закончил со вторым ПЗ

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from mainapp import views
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello_world),
-    path('<str:word>/', views.check_kwargs),
+    path('', RedirectView.as_view(url='mainapp/')),
+    path('mainapp/', include('mainapp.urls')),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
-
-# Create your views here.
+from re import template
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def hello_world(request):
-    return HttpResponse("Hello world!")
+class MainPageView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class NewsPageView(TemplateView):
+    template_name = 'mainapp/news.html'
+
+
+class CoursesPageView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+
+class ContactsPageView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+
+class DocSitePageView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+
+class LoginPageView(TemplateView):
+    template_name = 'mainapp/login.html'
 
 
 def check_kwargs(request, **kwargs):
-    return HttpResponse(f"kwargs:<br>{kwargs}")
+    return HttpResponse(f'kwargs:<br>{kwargs}')
